3cd73c93cb1c51032b0076d3bed86b3bb97592d4
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2017-03-13 21:29:59 -0300

    Changing models to SQLAlchemy

--- app/models.py
+++ app/models.py
@@ -10,98 +10,145 @@
 """
 # Standard lib imports
 from random import choice
+import json
 # Third-party imports
+from flask_sqlalchemy import SQLAlchemy
 # LeCoVi imports
 
 
-class Tile:
+db = SQLAlchemy()
+
+
+class GameOverException(Exception):
+    pass
+
+
+class Tile(db.Model):
     """This class will represent each Tile."""
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.has_mine = False
-        self.is_revealed = False
-        self.is_marked = False
-        self.mines_around = 0
+    __tablename__ = 'tiles'
 
-    def get_neighbours(self, board):
-        neighbours_list = list()
-        if self.y == 0:
-            neighbours_list.append(board.tiles[self.x][self.y + 1])
-            if self.x == 0:
-                neighbours_list.append(board.tiles[self.x + 1][self.y])
-                neighbours_list.append(board.tiles[self.x + 1][self.y + 1])
-            elif self.x == board.width - 1:
-                neighbours_list.append(board.tiles[self.x - 1][self.y])
-                neighbours_list.append(board.tiles[self.x - 1][self.y + 1])
-            else:
-                neighbours_list.append(board.tiles[self.x - 1][self.y])
-                neighbours_list.append(board.tiles[self.x + 1][self.y])
-                neighbours_list.append(board.tiles[self.x - 1][self.y + 1])
-                neighbours_list.append(board.tiles[self.x + 1][self.y + 1])
-        elif self.y == board.height - 1:
-            neighbours_list.append(board.tiles[self.x][self.y - 1])
-            if self.x == 0:
-                neighbours_list.append(board.tiles[self.x + 1][self.y - 1])
-                neighbours_list.append(board.tiles[self.x + 1][self.y])
-            elif self.x == board.width - 1:
-                neighbours_list.append(board.tiles[self.x - 1][self.y - 1])
-                neighbours_list.append(board.tiles[self.x - 1][self.y])
-            else:
-                neighbours_list.append(board.tiles[self.x - 1][self.y - 1])
-                neighbours_list.append(board.tiles[self.x + 1][self.y - 1])
-                neighbours_list.append(board.tiles[self.x - 1][self.y])
-                neighbours_list.append(board.tiles[self.x + 1][self.y])
-        else:
-            neighbours_list.append(board.tiles[self.x][self.y - 1])
-            neighbours_list.append(board.tiles[self.x][self.y + 1])
-            if self.x == 0:
-                neighbours_list.append(board.tiles[self.x + 1][self.y - 1])
-                neighbours_list.append(board.tiles[self.x + 1][self.y])
-                neighbours_list.append(board.tiles[self.x - 1][self.y + 1])
-            elif self.x == board.width - 1:
-                neighbours_list.append(board.tiles[self.x - 1][self.y - 1])
-                neighbours_list.append(board.tiles[self.x - 1][self.y])
-                neighbours_list.append(board.tiles[self.x - 1][self.y + 1])
-            else:
-                neighbours_list.append(board.tiles[self.x - 1][self.y - 1])
-                neighbours_list.append(board.tiles[self.x + 1][self.y - 1])
-                neighbours_list.append(board.tiles[self.x - 1][self.y])
-                neighbours_list.append(board.tiles[self.x + 1][self.y])
-                neighbours_list.append(board.tiles[self.x - 1][self.y + 1])
-                neighbours_list.append(board.tiles[self.x + 1][self.y + 1])
-        return neighbours_list
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    x = db.Column(db.Integer, nullable=False)
+    y = db.Column(db.Integer, nullable=False)
+    has_mine = db.Column(db.Boolean, default=False, nullable=False)
+    is_revealed = db.Column(db.Boolean, default=False, nullable=False)
+    is_marked = db.Column(db.Boolean, default=False, nullable=False)
+    mines_around = db.Column(db.Integer, default=0, nullable=False)
+    board_id = db.Column(db.ForeignKey('boards.id'))
 
 
-class Board:
+class Board(db.Model):
     """This class will represent the whole Board, a set of class:`Tile`"""
 
-    def __init__(self, width=15, height=15, mines=30):
-        self.width = width
-        self.height = height
-        self.mines = mines
-        self.tiles = list()
-        self._new_board()
+    __tablename__ = 'boards'
 
-    def _new_board(self):
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    width = db.Column(db.Integer, nullable=False, default=15)
+    height = db.Column(db.Integer, nullable=False, default=15)
+    mines = db.Column(db.Integer, nullable=False, default=30)
+
+    tiles = list()
+
+    @classmethod
+    def get_by(cls, board_id):
+        board = db.session.query(Board).filter_by(id=board_id).first()
+        board.tiles = list()
+        for x in range(board.width):
+            board.tiles.append(list())
+            for y in range(board.height):
+                tile = board.get_tile(x=x, y=y)
+                board.tiles[x].append(tile)
+        return board
+
+    def create_tiles(self):
         for x in range(self.height):
             self.tiles.append(list())
             for y in range(self.width):
-                new_tile = Tile(x=x, y=y)
+                new_tile = Tile(x=x, y=y, board_id=self.id)
+                db.session.add(new_tile)
+                db.session.commit()
                 self.tiles[x].append(new_tile)
+
+    def get_tile(self, x, y):
+        return db.session.query(Tile).filter_by(board_id=self.id, x=x,
+                                                y=y).first()
 
     def set_mines(self):
         mines_left = self.mines
         while mines_left > 0:
             random_row = choice(range(self.width))
             random_col = choice(range(self.height))
-            if not self.tiles[random_row][random_col].has_mine:
-                self.tiles[random_row][random_col].has_mine = True
+            tile = self.get_tile(x=random_row, y=random_col)
+            if not tile.has_mine:
+                tile.has_mine = True
             mines_left -= 1
 
     def set_mines_around(self):
         for row in self.tiles:
             for tile in row:
-                for neighbour in tile.get_neighbours(board=self):
+                for neighbour in self.get_neighbours(tile=tile):
                     if neighbour.has_mine:
                         tile.mines_around += 1
+
+    def get_neighbours(self, tile):
+        neighbours_list = list()
+        if tile.y == 0:
+            neighbours_list.append(self.get_tile(tile.x, tile.y + 1))
+            if tile.x == 0:
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y + 1))
+            elif tile.x == self.width - 1:
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y))
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y + 1))
+            else:
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y))
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y + 1))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y + 1))
+        elif tile.y == self.height - 1:
+            neighbours_list.append(self.get_tile(tile.x, tile.y - 1))
+            if tile.x == 0:
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y - 1))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y))
+            elif tile.x == self.width - 1:
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y - 1))
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y))
+            else:
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y - 1))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y - 1))
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y))
+        else:
+            neighbours_list.append(self.get_tile(tile.x, tile.y - 1))
+            neighbours_list.append(self.get_tile(tile.x, tile.y + 1))
+            if tile.x == 0:
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y - 1))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y + 1))
+            elif tile.x == self.width - 1:
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y - 1))
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y))
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y + 1))
+            else:
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y - 1))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y - 1))
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y))
+                neighbours_list.append(self.get_tile(tile.x - 1, tile.y + 1))
+                neighbours_list.append(self.get_tile(tile.x + 1, tile.y + 1))
+        return neighbours_list
+
+    def reveal_zone(self, tile):
+        neighbours = self.get_neighbours(tile=tile)
+        for neighbour in neighbours:
+            if not neighbour.is_revealed:
+                self.reveal_tile(x=neighbour.x, y=neighbour.y)
+
+    def reveal_tile(self, x, y):
+        tile = self.get_tile(x, y)
+        if not tile.is_marked and not tile.is_revealed:
+            if tile.has_mine:
+                raise GameOverException('boom!')
+            tile.is_revealed = True
+            if tile.mines_around == 0:
+                self.reveal_zone(tile)
